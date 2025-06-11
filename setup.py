92cb1d8f068b0cca85e3b5d0e0bf82cd91bae5d9
Lucas Antonio <lucas@arqlamp.com>
2025-06-11 09:49:04 -0300

    Revisão de todo o diretório, simplificando a organização, validação e preparação do modulo interno e teste de funcionalidade final. OK

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
     install_requires=[
         'pandas>=2.0',
         'openpyxl>=3.0',
+        'argparse',
+        'rastreador_xlsx',
+        'requests',
+        'sqlalchemy',
+        'sys',
+        'tqdm',
         # Adicione apenas dependências externas aqui
     ],
     python_requires='>=3.8',
